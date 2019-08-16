e87c562f66711a265f9f4cff9709075a0f3ecdef
Adrian <adriandev0353@outlook.com>
2019-08-16 11:12:03 +0200

    basic server setup

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
+
+const app = express();
+
+const pg = require('pg');
+const Pool = pg.Pool;
+
+let useSSL = false;
+const local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/hangman';
+
+const pool = new Pool({
+    connectionString,
+    ssl: useSSL
+});
+
+app.use(session({
+    secret: 'yikes',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+const handlebarSetup = exphbs({
+    partialsDir: './views',
+    viewPath: './views',
+    layoutsDir: './views/layouts'
+});
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('App started on port:', PORT);
+});
---
+++ tables.sql
@@ -0,0 +1,13 @@
+create table word_list(
+    id int not null,
+    word text not null,
+    word_length int not null
+);
+
+create table user_data(
+    id int not null,
+    username text not null,
+    password text not null,
+    points int not null,
+    words_played text not null
+);
