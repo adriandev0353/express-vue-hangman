9272fa70416a08600e5996da5470f2dff7d2b7a6
Adrian <adriandev0353@outlook.com>
2019-08-19 10:11:00 +0200

    API testing added

--- app-routing.js
+++ app-routing.js
@@ -2,8 +2,8 @@ module.exports = (app, hangmanAPI) => {
     app.get('/api/all/words', hangmanAPI.allWords);
     app.get('/api/all/users', hangmanAPI.allUsers);
     app.get('/api/list/size/:size', hangmanAPI.listWordOfSize);
-    app.get('/api/add/word/:word', hangmanAPI.addNewWord);
     app.post('/api/add/word/:word', hangmanAPI.addNewWord);
     app.post('/api/add/user', hangmanAPI.addUser);
+    app.post('/api/add/to/user', hangmanAPI.addWordTo);
     app.post('/api/dec/points', hangmanAPI.decrementPoints);
 };
--- test/hangmanAPI.spec.js
+++ test/hangmanAPI.spec.js
@@ -17,11 +17,70 @@ describe('API Tests', () => {
     AppRouting(app, hangmanMockApi);
 
     describe('Testing fetching APIs that return lists from the database', () => {
-        it('should return a list of all records in the database', (done) => {
+        it('should return a list of all words in the database', (done) => {
+            request(app)
+                .get('/api/all/words')
+                .end((err, res) => {
+                    expect(res.body.words).to.be.deep.equal('success');
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a list of all users in the database', (done) => {
+            request(app)
+                .get('/api/all/users')
+                .end((err, res) => {
+                    expect(res.body.words).to.be.deep.equal('success');
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a list of all words the with a length of 2', (done) => {
             request(app)
                 .get('/api/list/size/2')
                 .end((err, res) => {
                     expect(res.body.words).to.be.deep.equal('success');
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a successful posting of a new word', (done) => {
+            request(app)
+                .post('/api/add/word/suspicious')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a successful posting of a user', (done) => {
+            request(app)
+                .post('/api/add/user')
+                .send({ username: 'dyllanhope', password: '123' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a successful posting of adding a winning word to a user', (done) => {
+            request(app)
+                .post('/api/add/to/user')
+                .send({ username: 'dyllanhope', word: 'suspicious' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('should return a successful posting of decrementing points from a user', (done) => {
+            request(app)
+                .post('/api/dec/points')
+                .send({ username: 'dyllanhope', points: 10 })
+                .end((err, res) => {
                     expect(res.body.status).to.be.equal('success');
                     expect(res.statusCode).to.be.equal(200);
                     done();
