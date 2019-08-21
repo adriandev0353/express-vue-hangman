be279a81bf49142a7986ac5d50ac2d75224bd1f4
Adrian <adriandev0353@outlook.com>
2019-08-21 16:52:14 +0200

    check user api added

--- app-routing.js
+++ app-routing.js
@@ -5,7 +5,7 @@ module.exports = (app, hangmanAPI) => {
     app.get('/api/all/words', hangmanAPI.allWords);
     app.get('/api/all/users', hangmanAPI.allUsers);
     app.get('/api/list/size/:size', hangmanAPI.listWordOfSize);
-    app.get('/api/check/user/:user', hangmanAPI.checkUser);
+    app.get('/api/check/user/:user', hangmanAPI.userCheck);
     app.post('/api/add/word/:word', hangmanAPI.addNewWord);
     app.post('/api/add/user', hangmanAPI.addUser);
     app.post('/api/add/to/user', hangmanAPI.addWordTo);
