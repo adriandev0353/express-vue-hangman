9a73657626241230fe6b6337a2183894183c130d
Adrian <adriandev0353@outlook.com>
2019-09-03 10:56:05 +0200

    win rate user_data testing

--- hangman-service.js
+++ hangman-service.js
@@ -104,7 +104,7 @@ module.exports = (pool) => {
 
         if (allUsers.rowCount === 0) {
             const data = [1, username, password, 0];
-            await pool.query('INSERT INTO user_data(id, username, password, points) VALUES($1, $2, $3, $4)', data);
+            await pool.query('INSERT INTO user_data(id, username, password, points, win_rate) VALUES($1, $2, $3, $4, $5)', data);
         } else if (search.rowCount === 0) {
             const id = userList[userList.length - 1].id + 1;
             const data = [id, username, password, 0, 0];
