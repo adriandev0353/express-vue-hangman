b21828552e7e1316352b5be91c4b4dafa040df7f
Adrian <adriandev0353@outlook.com>
2019-09-25 11:46:32 +0200

    api tests updated

--- test/hangman-mock-service.js
+++ test/hangman-mock-service.js
@@ -32,55 +32,55 @@ module.exports = (pool) => {
         };
     };
 
-    const decrementPoints = async (username, points) => {
-        if (username && points) {
-            return 'success';
-        };
-    };
+    const allUsers = async () => { return 'success'; };
 
-    const allUsers = async () => {
-        return 'success';
-    };
+    const delUser = async () => { return 'success'; };
 
-    const delUser = async () => {
-        return 'success';
-    };
+    const loginCheck = async () => { return 'success'; };
 
-    const loginCheck = async () => {
-        return 'success';
-    };
+    const userCheck = async () => { return 'success'; };
 
-    const userCheck = async () => {
-        return 'success';
-    };
+    const personalData = async () => { return 'success'; };
 
-    const personalData = async () => {
-        return 'success';
-    };
+    const findUser = async () => { return 'success'; };
 
-    const findUser = async () => {
-        return 'success';
-    };
+    const checkWord = async () => { return 'success'; };
 
-    const checkWord = async () => {
-        return 'success';
-    };
+    const newWordList = async () => { return 'success'; };
 
-    const newWordList = async () => {
-        return 'success';
-    };
+    const setNewWordStatus = async () => { return 'success'; };
 
-    const setNewWordStatus = async () => {
-        return 'success';
-    };
+    const storeNewWord = async () => { return 'success'; };
 
-    const storeNewWord = async () => {
-        return 'success';
-    };
+    const choiceFilter = async () => { return 'success'; };
 
-    const choiceFilter = async () => {
-        return 'success';
-    };
+    const linkTableData = async () => { return 'success'; };
+
+    const checkWordsGuessed = async () => { return 'success'; };
+
+    const addFriends = async () => { return 'success'; };
+
+    const returnFriendRequests = async => { return 'success'; };
+
+    const friendList = async => { return 'success'; };
+
+    const confirmRequest = async => { return 'success'; };
+
+    const denyRequest = async => { return 'success'; };
+
+    const deleteFriend = async => { return 'success'; };
+
+    const sendChallenge = async => { return 'success'; };
+
+    const fetchChallengesFor = async => { return 'success'; };
+
+    const fetchChallengesSentBy = async => { return 'success'; };
+
+    const fetchCompleteChallenges = async => { return 'success'; };
+
+    const setChallengeStatus = async => { return 'success'; };
+
+    const removeChallenge = async => { return 'success'; };
 
     return {
         reloadData,
@@ -89,17 +89,30 @@ module.exports = (pool) => {
         addNewWord,
         allUsers,
         addUser,
-        addWordTo,
-        decrementPoints,
-        delUser,
-        loginCheck,
         userCheck,
+        addWordTo,
+        loginCheck,
         personalData,
+        choiceFilter,
         findUser,
+        delUser,
         checkWord,
+        storeNewWord,
         newWordList,
         setNewWordStatus,
-        storeNewWord,
-        choiceFilter
+        linkTableData,
+        checkWordsGuessed,
+        addFriends,
+        returnFriendRequests,
+        friendList,
+        confirmRequest,
+        denyRequest,
+        deleteFriend,
+        sendChallenge,
+        fetchChallengesFor,
+        fetchChallengesSentBy,
+        fetchCompleteChallenges,
+        setChallengeStatus,
+        removeChallenge
     };
 };
--- test/hangmanAPI.spec.js
+++ test/hangmanAPI.spec.js
@@ -17,7 +17,7 @@ describe('API Tests', () => {
     AppRouting(app, hangmanMockApi);
 
     describe('Testing fetching APIs that return lists from the database', () => {
-        it('should return a list of all words in the database', (done) => {
+        it('Should return a list of all words in the database', (done) => {
             request(app)
                 .get('/api/all/words')
                 .end((err, res) => {
@@ -27,7 +27,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a list of all users in the database', (done) => {
+        it('Should return a list of all users in the database', (done) => {
             request(app)
                 .get('/api/all/users')
                 .end((err, res) => {
@@ -37,7 +37,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a list of all words the with a length of 2', (done) => {
+        it('Should return a list of all words the with a length of 2', (done) => {
             request(app)
                 .get('/api/list/size/2')
                 .end((err, res) => {
@@ -47,7 +47,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of a new word', (done) => {
+        it('Should return a successful posting of a new word', (done) => {
             request(app)
                 .post('/api/add/word/from/user')
                 .send({ word: 'suspicious', user: 'dyllanhope' })
@@ -57,7 +57,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of a user', (done) => {
+        it('Should return a successful posting of a user', (done) => {
             request(app)
                 .post('/api/add/user')
                 .send({ username: 'dyllanhope', password: '123' })
@@ -67,7 +67,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of adding a winning word to a user', (done) => {
+        it('Should return a successful posting of adding a winning word to a user', (done) => {
             request(app)
                 .post('/api/add/to/user')
                 .send({ username: 'dyllanhope', word: 'suspicious' })
@@ -77,7 +77,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of deleting a user', (done) => {
+        it('Should return a successful posting of deleting a user', (done) => {
             request(app)
                 .post('/api/delete/user')
                 .send({ username: 1 })
@@ -87,7 +87,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return that the user login is unauthorized', (done) => {
+        it('Should return that the user login is unauthorized', (done) => {
             request(app)
                 .post('/api/login/check')
                 .send({ username: 'dyllanhope', password: '123' })
@@ -97,7 +97,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of checking for the user dyllanhope', (done) => {
+        it('Should return a successful posting of checking for the user dyllanhope', (done) => {
             request(app)
                 .get('/api/check/user/dyllanhope')
                 .end((err, res) => {
@@ -106,7 +106,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of finding dyllanhopes personal data (game history)', (done) => {
+        it('Should return a successful posting of finding dyllanhopes personal data (game history)', (done) => {
             request(app)
                 .get('/api/get/user/data/dyllanhope')
                 .end((err, res) => {
@@ -115,7 +115,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of finding the user dyllanhope', (done) => {
+        it('Should return a successful posting of finding the user dyllanhope', (done) => {
             request(app)
                 .get('/api/find/user/dyllanhope')
                 .end((err, res) => {
@@ -124,7 +124,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of checking if a word already exists', (done) => {
+        it('Should return a successful posting of checking if a word already exists', (done) => {
             request(app)
                 .get('/api/check/word/word')
                 .end((err, res) => {
@@ -133,7 +133,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a list of the new words added by users', (done) => {
+        it('Should return a list of the new words added by users', (done) => {
             request(app)
                 .get('/api/all/new/words')
                 .end((err, res) => {
@@ -142,7 +142,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of setting the new word statuses', (done) => {
+        it('Should return a successful posting of setting the new word statuses', (done) => {
             request(app)
                 .post('/api/set/new/word/status')
                 .send({ word: 'mouse', status: 'confirmed' })
@@ -152,7 +152,7 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a successful posting of storing a new word in the "new_words" table', (done) => {
+        it('Should return a successful posting of storing a new word in the "new_words" table', (done) => {
             request(app)
                 .post('/api/store/new/word')
                 .send({ word: 'mouse', user: 'dyllanhope' })
@@ -162,9 +162,143 @@ describe('API Tests', () => {
                     done();
                 });
         });
-        it('should return a filter of all the games dyllanhope has won', (done) => {
+        it('Should return a filter of all the games dyllanhope has won', (done) => {
             request(app)
                 .get('/api/get/user/data/user/dyllanhope/choice/won')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of data on all the words played', (done) => {
+            request(app)
+                .get('/api/link/data')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of adding a friend', (done) => {
+            request(app)
+                .post('/api/add/friends')
+                .send({ requester: 'michael', receiver: 'dyllanhope' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of friends for "michael"', (done) => {
+            request(app)
+                .get('/api/friend/list/michael')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of friend requests for "michael"', (done) => {
+            request(app)
+                .get('/api/friend/requests/for/michael')
+                .end((err, res) => {
+                    expect(res.body.result).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of requesting complete words by "michael"', (done) => {
+            request(app)
+                .post('/api/check/word/played')
+                .send({ user: 'michael', word: 'house' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of confirming a friend request', (done) => {
+            request(app)
+                .post('/api/confirm/friend/request')
+                .send({ requester: 'michael', receiver: 'dyllanhope' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of denying a friend request', (done) => {
+            request(app)
+                .post('/api/deny/friend/request')
+                .send({ requester: 'michael', receiver: 'dyllanhope' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it("Should return a successful posting of deleting michael's friend dyllanhope", (done) => {
+            request(app)
+                .post('/api/remove/friend')
+                .send({ user: 'michael', friend: 'dyllanhope' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of a challenge from michael to dyllanhope', (done) => {
+            request(app)
+                .post('/api/send/challenge')
+                .send({ challenger: 'michael', opponent: 'dyllanhope', word: 'house', hint: 'building' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of challenges sent to michael', (done) => {
+            request(app)
+                .get('/api/fetch/challenges/for/michael')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of challenges complete by michael', (done) => {
+            request(app)
+                .get('/api/fetch/complete/challenges/by/michael')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a list of challenges sent by john', (done) => {
+            request(app)
+                .get('/api/fetch/challenges/sent/by/john')
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting of setting  challenge status to "won"', (done) => {
+            request(app)
+                .post('/api/set/challenge/status')
+                .send({ opponent: 'dyllanhope', status: 'won', word: 'house' })
+                .end((err, res) => {
+                    expect(res.body.status).to.be.equal('success');
+                    expect(res.statusCode).to.be.equal(200);
+                    done();
+                });
+        });
+        it('Should return a successful posting ofremoving a challenge', (done) => {
+            request(app)
+                .post('/api/remove/challenge')
+                .send({ opponent: 'dyllanhope', word: 'house' })
                 .end((err, res) => {
                     expect(res.body.status).to.be.equal('success');
                     expect(res.statusCode).to.be.equal(200);
